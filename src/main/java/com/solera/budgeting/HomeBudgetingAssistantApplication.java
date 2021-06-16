9c83009850fbc210b69e31fa26264fd2478c870c
Marek Maciejewski <marekmaciejewski1985@gmail.com>
2021-06-16 22:34:55 +0200

    Project's base.

---
+++ src/main/java/com/solera/budgeting/HomeBudgetingAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.solera.budgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBudgetingAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeBudgetingAssistantApplication.class, args);
+    }
+}
